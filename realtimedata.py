fb2bf71f864f07711c8dfa21e9d4584158d26e01
millionPan <47236818+millionPan@users.noreply.github.com>
2023-04-01 23:19:22 +0800

    Update realtimedata.py

--- realtimedata.py
+++ realtimedata.py
@@ -25,7 +25,8 @@ import time
 import random
 import pandas as pd 
 
-import statsmodels.api
+import statsmodels.formula
+import statsmodels.formula.api
 #print(ts.__version__)
                 
 symbollist=['600588','600986','600728','600050','000070']
