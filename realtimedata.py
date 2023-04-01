db6e40af1f971ba56e455e46f4653ac7dd324f15
millionPan <809251311@qq.com>
2023-04-01 17:30:46 +0800

    try tushare2

--- realtimedata.py
+++ realtimedata.py
@@ -23,8 +23,8 @@ pro = ts.pro_api('e79d0344d6ac178e4d5973c42b612c9ed776bc47117c49aa9d3d7b24')
 
 import time
 import random
-import statsmodels.formula.api as smf
-import pandas as pd 
+#import statsmodels.formula.api as smf
+#import pandas as pd 
 #print(ts.__version__)
 # import os
 # st.write(os.getcwd())   #取得当前工作目录
